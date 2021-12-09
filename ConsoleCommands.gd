06c4c734d996b3a4c6bba33727a7c210f64908dc
aefreedman <aaron@secretcrush.net>
2021-12-09 10:29:55 -0500

    basic commands setup

--- ConsoleCommands.gd
+++ ConsoleCommands.gd
@@ -1,10 +1,30 @@
 extends Control
 
-func print_hello(name = ''):
-	Console.write_line('Hello ' + name + '!')
+# Console stuff
+
+func set_name(name = ''):
+	Console.write_line('Welcome aboard the ' + name + '!')
+	shipName = name
+	
+func get_pos():
+	Console.write_line(shipPos)
 	
 func _ready():
-	Console.add_command('sayHello', self, 'print_hello')\
-	.set_description('Prints "Hello %name%"')\
+	Console.toggle_console()
+	Console.add_command('name', self, 'set_name')\
+	.set_description('Sets name to "%name%"')\
 	.add_argument('name', TYPE_STRING)\
 	.register()
+	
+	Console.add_command('pos', self, 'get_pos')\
+	.set_description('Get ship pos')\
+	.register()
+
+# Game Stuff
+
+var shipPos = Vector2(0, 0)
+var shipVel = Vector2(1, 1)
+var shipName = ''
+
+func _process(delta):
+	shipPos += shipVel * delta
